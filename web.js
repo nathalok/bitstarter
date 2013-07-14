8099f8be83d22c4b1a46615eaa58769fc2dceacb
Ubuntu <ubuntu@ip-172-31-40-63.us-west-2.compute.internal>
2013-07-14 13:03:14 +0000

    fixed crash

--- web.js
+++ web.js
@@ -2,14 +2,16 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var buffer;
 app.get('/', function(request, response) {
     fs.readFile('./index.html', 'utf8', function(err,data){
 	if(err)throw err;
+	buffer = data.toString();
 	console.log(data);
     });
     
     //response.send('Hello World v2!');
-    response.send(data);
+    response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
