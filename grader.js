9386a792f2530ce448a37972ae2a2beb5a1ccb87
Ubuntu <ubuntu@ip-172-31-40-63.us-west-2.compute.internal>
2013-07-17 12:47:24 +0000

    updated for any url

--- grader.js
+++ grader.js
@@ -9,11 +9,14 @@ References:
 + commander.js
 
 */
+var sys = require('util');
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+
+var rest = require('restler');
 
 var assertFileExists = function(infile){
     var instr = infile.toString();
@@ -32,10 +35,36 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
+
+ var checkHtmlFile = function(myURL, checksfile) {
+     var out = {};
+     rest.get(myURL).on('complete', function(result){ 
+	fs.writeFileSync(HTMLFILE_DEFAULT, result);
+
+	$ = cheerioHtmlFile(HTMLFILE_DEFAULT);
+	var checks = loadChecks(checksfile).sort();
+
+	for(var ii in checks) {
+	    
+	    var present = $(checks[ii]).length > 0;
+	    out[checks[ii]] = present;
+	    
+	}
+//	 console.log("Raw out is ", out);
+	 console.log(JSON.stringify( out, null, 4));
+
+    });
+};
+
+
+ var _checkHtmlFile = function(myURL, htmlfile, checksfile) {
+    console.log("inside checkHtmlFile");
+    getHtmlFile(myURL, htmlfile);
+
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
+
     for(var ii in checks) {
 	var present = $(checks[ii]).length > 0;
 	out[checks[ii]] = present;
@@ -51,14 +80,17 @@ var clone = function(fn) {
 if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+//	.option('-f, --file <html_file>', 'Path to index.html')//, clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_file>', 'bitstarter url for index.html')//, clone(assertFileExists), HTMLFILE_DEFAULT)
 	.parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    checkHtmlFile(program.url, program.checks);
+
+    //console.log("json sring is.. ", checkJson);
+    //var outJson = JSON.stringify(checkJson, null, 4);
+    //console.log(outJson);
 }else{
     exports.checkHtmlFile = checkHtmlFile;
 }
-//assessertFileExists("index.html");
-//cheerioHtmlFile("index.html");
-//loadChecks("index.html");
+
+
